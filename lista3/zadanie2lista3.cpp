57e1fc4f375ba67ad64e025a97b6466a2a820932
akojtych <62288368+akojtych@users.noreply.github.com>
2020-04-21 12:04:20 +0200

    Add files via upload

---
+++ lista3/zadanie2lista3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //podpunkt A
+    double(*x)[100];
+    //podpunkt B
+    char * array[5][5];
+    //podpunkt C
+    char * array[];
+    //podpunkt D
+    vector <void*> wektor;
+    //podpunkt E
+    vector <double> wektor[5];
+    //podpunkt F
+    double (*func)(double*, int)
+    //podpunkt G
+    int *g;
+    int funkcja(*g);
+    return 0;
+}
